f0eea0f88ac975cabc7eac6e903e3aec33ee9762
tato88 <380685013386@ex.ua>
2022-06-27 23:13:37 +0300

    fix

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-from first.views import FirstView,SecondView,ThirdView
-from hw1.views import HomeWorkView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
